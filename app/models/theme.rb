132ce26bc91fdf47b9c6f76d91652cc211ce9fcc
kentoshiohara <120357291+kentoshiohara@users.noreply.github.com>
2023-02-08 15:05:05 +0900

    モデル情報修正

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,7 +1,8 @@
 class Theme < ActiveHash::Base
   self.data = [
     { id: 1, name: 'グルメ'}, { id: 2, name: '温泉'},
-    { id: 3, name: 'リゾート'}, { id: 4, name: 'テーマパーク'}
+    { id: 3, name: 'リゾート'}, { id: 4, name: 'テーマパーク'},
+    { id: 5, name: 'その他'}
   ]
   include ActiveHash::Associations
   has_many :destinations

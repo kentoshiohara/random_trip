003a14de41f750669109abdb3cf9299b5ad5957f
kentoshiohara <120357291+kentoshiohara@users.noreply.github.com>
2023-02-09 23:06:16 +0900

    旅行先画像保存

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -5,5 +5,5 @@ class Destination < ApplicationRecord
   belongs_to :member
   belongs_to :season
 
-  validates :prefecture_id, :theme_id, :member_id, :season_id, numericality: {other_than: 1, message: "can't be blank"}
+  validates :prefecture_id, :theme_id, :member_id, :season_id, presence: true
 end

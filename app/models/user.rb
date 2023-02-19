2337258796deda36c88abfc12c79c0e92af2a043
kentoshiohara <120357291+kentoshiohara@users.noreply.github.com>
2023-02-19 11:58:37 +0900

    単体テストコード記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,9 @@ class User < ApplicationRecord
   has_many :user_histories
   has_many :histories, through: :user_histories
 
+  validates :name, presence: true, length: { maximum: 8 }
+
   def self.history_record(user_histories)
-    # history_all = History.all.count(:id)
     history_true = user_histories
     history_false = (47 - history_true)
     [{ '訪問済み': history_true, '未訪問': history_false }, history_true]
